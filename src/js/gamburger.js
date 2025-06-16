49560cd44f73d7e08e4e64079f4fc96283114a06
Mike <mikeduchenko@gmail.com>
2025-06-16 08:38:12 +0300

    menu

--- src/js/gamburger.js
+++ src/js/gamburger.js
@@ -1,6 +1,5 @@
 const menuBtn = document.getElementById("menu-btn");
 const navMenu = document.querySelector(".header__bottom");
-console.log(navMenu);
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navMenu.classList.toggle("active");
